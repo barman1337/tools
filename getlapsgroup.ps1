29a8cdc36d5bb316d9ce7e9e7703a5f00d13db3b
barman1337 <59201210+barman1337@users.noreply.github.com>
2025-08-03 01:59:25 +0300

    Update getlapsgroup.ps1

--- getlapsgroup.ps1
+++ getlapsgroup.ps1
@@ -1,4 +1,4 @@
-$computerName = "MY-COMPUTER"
-$comp = [ADSI]"LDAP://CN=$computerName,OU=Computers,DC=example,DC=com"
-$acl = $comp.psbase.ObjectSecurity.Access
-$acl | Where-Object { $_.ObjectType -eq "c90f336d-0b34-4d50-8a49-63b4a7f7e731" -and $_.ActiveDirectoryRights -match "ReadProperty" }
+Get-Acl "AD:\CN=ComputerName,OU=YourOU,DC=domain,DC=com" |
+    Select-Object -ExpandProperty Access |
+    Where-Object { $_.ActiveDirectoryRights -match "ReadProperty" -and $_.ObjectType -eq "bf967a86-0de6-11d0-a285-00aa003049e2" } |
+    Select-Object IdentityReference, ActiveDirectoryRights, ObjectType
